de977f7cba3a037f6706264d405dd6ce76741e98
Fatbardh Durmishi <fdurmishi789@gmail.com>
2022-04-30 01:15:24 +0200

    Add files via upload

---
+++ MenaxhimiKafiteris.BO/Rolet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenaxhimiKafiteris.BO
+{
+    class Rolet
+    {
+
+        public int roliID { get; set; }
+        public string roli { get; set; }
+    }
+}
