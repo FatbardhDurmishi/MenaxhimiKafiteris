ebdf25f6b108c5f04f453a1925f58c6910554350
Fatbardh Durmishi <fdurmishi789@gmail.com>
2022-04-30 15:30:15 +0200

    Add files via upload

---
+++ MenaxhimiKafiteris.BO/PerbersitEProdukteve.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenaxhimiKafiteris.BO
+{
+    public class PerbersitEProdukteve
+    {
+        public int ProduktiID { get; set; }
+        public int PerbersiID { get; set; }
+        public decimal SasiaPerbersit { get; set; }
+    }
+}
